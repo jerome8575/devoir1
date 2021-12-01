cb23e1dbd715b8955802c7daf90f3939c5833289
algorealm <80223269+algorealm@users.noreply.github.com>
2021-11-30 22:32:53 -0500

    Add files via upload

---
+++ packageDev1/DevControle.java
@@ -0,0 +1,83 @@
+package packageDev1;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.*;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.text.*;
+
+public class DevControle implements MouseListener, ActionListener, CaretListener{
+    DevAffichage monAffichage;
+    DevModele monModel;
+
+    public DevControle(DevAffichage x, DevModele y)
+    {
+        monAffichage = x;
+        monModel = y;
+    }
+
+    public void caretUpdate(CaretEvent e)
+        {
+            int caretPosition = monAffichage.tArea.getCaretPosition();
+            Element root = monAffichage.tArea.getDocument().getDefaultRootElement();
+            int row = root.getElementIndex( caretPosition );
+            int column = caretPosition - root.getElement( row ).getStartOffset();
+            System.out.println( "Row   : " + ( row + 1 ) );
+            System.out.println( "Column: " + ( column + 1 ) );
+        }
+
+    public void mouseClicked(MouseEvent e) {
+        if ( SwingUtilities.isRightMouseButton(e) ){
+            monModel.wordClicked(monAffichage.tArea.viewToModel( e.getPoint() ));
+        }
+    }
+
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand() == "Importer les mots")
+        {
+            monModel.ouvrirFichier("importMots");
+            //monModel.dictioImport.checkIfIn(monModel.motsImport.arToStr);
+        }
+        else if(e.getActionCommand() == "Importer un dictionnaire")
+        {
+            monModel.ouvrirFichier("importDictio");
+        }
+
+        else if(e.getActionCommand() == "Sauvegarder les corrections")
+        {
+            monModel.saveFile();
+        }
+
+        else if (e.getActionCommand() == "Corriger")
+        {
+
+            monModel.corrigerText(monAffichage.tArea.getText());
+        }
+
+        //changer les mots
+        else 
+        {
+            monModel.changeWord(e.getActionCommand());
+            System.out.print(e.getActionCommand());
+        }
+    }
+}
---
+++ packageDev1/DevModele.java
@@ -0,0 +1,169 @@
+package packageDev1;
+
+import javax.swing.*;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Highlighter;
+import javax.swing.text.Utilities;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class DevModele extends JFrame{
+    Dictionnaire dictioImport; //Dictionnary imported by user
+    Dictionnaire motsImport; //Text imported by user
+    DevAffichage monAffichage;
+    String absolutePath;
+    String word;
+
+    ArrayList<MotIncorrect> motsIncorrects;
+
+    public DevModele(DevAffichage test)
+    {
+        monAffichage = test;
+        motsIncorrects = new ArrayList<MotIncorrect>();
+    }
+
+    //highlight particular word in textarea
+    public void redHightligher(ArrayList<MotIncorrect> motsIncorrects, String text)
+    {
+        try
+        {
+            int carretStart = 0;
+            int carretEnd = 0;
+            Highlighter.HighlightPainter redPainter = new DefaultHighlighter.DefaultHighlightPainter(Color.red);
+            for (int i = 0; i < motsIncorrects.size(); i++)
+            {
+                carretStart = motsImport.arrayTab.get(motsIncorrects.get(i).index);
+                carretEnd = carretStart + motsImport.arToStr.get(motsIncorrects.get(i).index).length();
+                monAffichage.tArea.getHighlighter().addHighlight(carretStart, carretEnd, redPainter);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.print("PLACEHOLDER4");
+        }
+
+    }
+
+    public void wordClicked(int offset)
+    {
+        try
+        {
+            System.out.println( monAffichage.tArea.modelToView( offset ) );
+            int start = Utilities.getWordStart(monAffichage.tArea,offset);
+            int end = Utilities.getWordEnd(monAffichage.tArea, offset);
+            word = monAffichage.tArea.getDocument().getText(start, end-start);
+
+
+
+            System.out.println( "Selected word: " + word);
+            int rowStart = Utilities.getRowStart(monAffichage.tArea, offset);
+            int rowEnd = Utilities.getRowEnd(monAffichage.tArea, offset);
+            System.out.println( "Row start offset: " + rowStart );
+            System.out.println( "Row end   offset: " + rowEnd );
+            monAffichage.tArea.select(rowStart, rowEnd);
+
+            // recommander des mots
+
+            recommander(word);
+
+        }
+        catch (Exception e2) {}
+    }
+
+    public void changeWord(String replacer)
+    {
+        motsImport.replaceWord(replacer, word);
+        motsImport.toArrays();
+        monAffichage.tArea.setText(motsImport.arrLiToStr());
+        corrigerText(monAffichage.tArea.getText());
+    }
+
+    public void saveFile()
+    {
+        try{
+            FileWriter filewriter = new FileWriter(absolutePath);
+            BufferedWriter writer = new BufferedWriter(filewriter);
+            for (int i = 0; i<motsImport.arToStr.size(); i++)
+            {
+                System.out.print(motsImport.arToStr.get(i));
+                writer.write(motsImport.arToStr.get(i) + "\n");
+            }
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            System.out.print("PLACEHOLDEr");
+        }
+
+    }
+
+    //for the
+    public void ouvrirFichier(String type)
+    {
+        try
+        {
+            JFileChooser filePicker = new JFileChooser();
+            filePicker.showOpenDialog(null);
+            absolutePath = filePicker.getSelectedFile().getAbsolutePath();
+            String fileName = filePicker.getSelectedFile().getName();
+
+            if (type.equals("importDictio"))
+            {
+                dictioImport = new Dictionnaire(absolutePath, fileName);
+                dictioImport.toArrays();
+                monAffichage.labelDictio.setText(dictioImport.nomFichier);
+            }
+
+            else if (type.equals("importMots"))
+            {
+
+                motsImport = new Dictionnaire(absolutePath, fileName);
+                motsImport.toArrays();
+                monAffichage.tArea.setText(motsImport.arrLiToStr());
+                monAffichage.labelMots.setText(motsImport.nomFichier);
+                motsImport.carretInd();
+                //ArrayList<MotIncorrect> motsIncorrects = dictioImport.checkIfIn(motsImport.arToStr);
+                //redHightligher(motsIncorrects);
+            }
+            
+        }
+
+        catch(Exception e)
+        {
+            System.err.print("PLACEHOLDER1");
+        }
+    }
+
+    public void corrigerText(String text){
+        // corriger text
+        System.out.println("Corriger text: " + text);
+        Parser parser = new Parser();
+        ArrayList<String> parsedTextArr = parser.parse(text);
+
+        for (String mot : parsedTextArr){
+            System.out.print(mot + " , ");
+        }
+
+        motsIncorrects = dictioImport.checkIfIn(parsedTextArr);
+        redHightligher(motsIncorrects, text);
+    }
+
+    public void recommander(String mot){
+        for (MotIncorrect motIncorrect : motsIncorrects){
+            if (motIncorrect.mot.equals(mot)){
+                monAffichage.afficherMenuRecommandation(motIncorrect.recommendations);
+            }
+        }
+        // do nothing
+    }
+
+
+}
